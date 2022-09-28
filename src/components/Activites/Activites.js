d52143dbcdba576c2db732c30ed555a20787908a
Nafis-Fahim <nafisf96@gmail.com>
2022-09-29 02:17:39 +0600

    display data on UI

--- src/components/Activites/Activites.js
+++ src/components/Activites/Activites.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import Activity from '../Activity/Activity';
 
 const Activites = ({activites}) => {
-    console.log(activites)
+    // console.log(activites)
     return (
         <div>
             <h1 className='font-bold mt-10'>Select Today's Activites</h1>
             {
-                activites.map(activity => <li>activity</li>)
+                activites.map(activity => <Activity
+                key={activity.id}
+                activity ={activity}
+                ></Activity> )
             }
         </div>
     );
--- src/components/Activity/Activity.js
+++ src/components/Activity/Activity.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
-const Activity = () => {
+const Activity = ({activity}) => {
+    const {name,img,time} = activity;
     return (
         <div>
-            
+            <div>
+                <img src={img} alt="" />
+                <h4>Name: {name}</h4>
+                <p>Time: {time}</p>
+            </div>
         </div>
     );
 };
