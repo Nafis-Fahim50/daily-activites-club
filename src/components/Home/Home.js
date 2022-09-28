6b1f857d62fbffdaa4afd31db10c4d18b62e6ed8
Nafis-Fahim <nafisf96@gmail.com>
2022-09-29 03:10:02 +0600

    added break time btn

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -20,7 +20,22 @@ const Home = () => {
                     ></Activites>
                 </div>
                 <div className='bg-white'>
-                    This is Cart Section
+                    <div className='mt-5 text-center'>
+                        <h1 className='font-semibold'>Nafis Fahim</h1>
+                        <p>Rajshahi,Bangladesh</p>
+                    </div>
+
+                    <div className='mt-12  ml-5'>
+                        <h1 className='font-bold'>Add a break</h1>
+                        <div className='bg-slate-100 mr-5 rounded-lg p-3 mt-3'>
+                            <button className='rounded-full text-black bg-white p-2 ml-4'>20s</button>
+                            <button className='rounded-full text-black bg-white p-2 ml-3'>30s</button>
+                            <button className='rounded-full text-black bg-white p-2 ml-3'>40s</button>
+                            <button className='rounded-full text-black bg-white p-2 ml-3'>50s</button>
+                        </div>
+                        
+                    </div>
+                    
                 </div>
             </div>
         </div>
